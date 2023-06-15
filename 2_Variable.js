93b5ac949eb3dad02546890f5f170c34a29b498c
Sohanur <39675963+fr2sr@users.noreply.github.com>
2023-06-15 17:59:25 +0600

    Variable Practice

---
+++ 2_Variable.js
@@ -0,0 +1,17 @@
+var name = "Sohanur Rahman";
+
+console.log("Welcome " + name);
+
+var firstNumber = 11;
+var lastNumber = 5;
+
+
+console.log("Addition: ", firstNumber + lastNumber);
+
+console.log("Subtraction: ", firstNumber - lastNumber);
+
+console.log("Multiplication: ", firstNumber * lastNumber);
+
+console.log("Division: ", firstNumber / lastNumber);
+
+console.log("Modulation: ", firstNumber % lastNumber);
